2c46450c126f0c960274e6048ca73767ec04c46d
sauron <1369130123@qq.com>
2019-03-18 15:21:33 +0000

    finish part of monitor and controller

---
+++ resourceControll/cgroup.py
@@ -0,0 +1,24 @@
+from cgroupspy import trees
+import subprocess
+
+def createCgroup(subsystem,name):
+    sS = trees.Tree().get_node_by_path('/'+str(subsystem)+'/')
+    if sS.create_cgroup(name) is None:
+        print('Err: Wrong Path:',str(subsystem)+str(name)+'/')
+
+def addProcs(proId,path):
+    if subprocess.getstatusoutput('echo '+str(proId)+' > /sys/fs/cgroup/'+str(path)+'/cgroup.procs') == 1:
+        print('Err: Add Process Fail')
+
+def quotaC(name,quota):
+    t = trees.Tree()
+    app = t.get_node_by_path('/cpu/'+str(name)+'/')
+    if app is None:
+        print('Err: Wrong Path:','/cpu/'+str(name)+'/')
+        return
+    app.controller.cfs_quota_us = int(quota)
+
+if __name__ == '__main__':
+    addProcs(input("pid:"),input("path"))
+    #createCgroup(input('subsystem:'),input('name:'))
+    #quotaC(input("appName:"),input(" quota value:"))
